5420c954bfa11268dd1ef1e34b428546edc824a0
vsn_20 <vuppalasainaman@gmail.com>
2025-12-01 20:19:30 +0530

    salary feature added in admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -42,6 +42,7 @@ import ManagerInventoryPage from './pages/manager/Inventory/ManagerInventoryPage
 import Manager_Sales from './pages/manager/Sales/Manager_Sales';
 import AdminOrders from './pages/owner/Orders/AdminOrders';
 import Admin_Inventory from './pages/owner/Inventory/Admin_Inventory';
+import { Admin_salary } from './pages/owner/Salary/Admin_salary';
 const PostLoginRedirect = () => {
     const { user } = useContext(AuthContext);
 
@@ -93,6 +94,7 @@ function App() {
                         <Route path="/owner/sales" element={<ProtectedRoute roles={['owner']}><OwnerSales/></ProtectedRoute>} />
                         <Route path="/owner/orders" element={<ProtectedRoute roles={['owner']}><AdminOrders /></ProtectedRoute>} />
                         <Route path="/owner/inventory" element={<ProtectedRoute roles={['owner']}><Admin_Inventory /></ProtectedRoute>} />
+                        <Route path="/owner/salaries" element={<ProtectedRoute roles={['owner']}><Admin_salary /></ProtectedRoute>} />
 
                         {/* Manager Routes */}
                         <Route path="/manager/analytics" element={<ProtectedRoute roles={['manager']}><ManagerAnalyticsPage /></ProtectedRoute>} />
