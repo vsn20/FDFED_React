dc2ea3e361f5e2c1c94fbde6a48ad88f83382942
“[VenkataHemaSai]” <[venkatahemasaibhimineni@gmail.com]>
2026-02-10 14:54:58 +0530

    image changed

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -64,7 +64,7 @@ const LoginPage = () => {
     };
 
     // Images from your EJS file
-    const loginImage = "https://plus.unsplash.com/premium_photo-1664476845274-27c2dabdd7f0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8c3RvY2slMjBtYXJrZXR8ZW58MHx8MHx8fDA%3D";
+    const loginImage = "employee.jpg";
     const signupImage = "https://images.unsplash.com/photo-1542744173-8e7e53415bb0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3";
 
     return (
